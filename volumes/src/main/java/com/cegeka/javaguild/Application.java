2b29f54d6bf57fc28155268c3941a3935c9f645a
tomjo <tom.jorissen@cegeka.com>
2023-04-19 17:42:59 +0200

    Add some more about volumes

--- volumes/src/main/java/com/cegeka/javaguild/Application.java
+++ volumes/src/main/java/com/cegeka/javaguild/Application.java
@@ -3,10 +3,15 @@ package com.cegeka.javaguild;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.time.LocalDateTime;
+
 @SpringBootApplication
 public class Application {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
         SpringApplication.run(Application.class, args);
         System.out.println("Hello Java Guild");
+        Files.writeString(Path.of("/data/hello.txt"), "Hello at " + LocalDateTime.now());
     }
 }
